6ef7d57ade76651691e662183f2089ddb53b0347
alexbtn <alexbonteanu22@gmail.com>
2021-04-12 13:04:44 +0300

    A and B implemented

---
+++ PracticalTest01Var03/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var03/PracticalTest01Var03MainActivity.java
@@ -0,0 +1,87 @@
+package ro.pub.cs.systems.eim.practicaltest01var03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class PracticalTest01Var03MainActivity extends AppCompatActivity {
+    private Button plusButton, minusButton, goToSecondary;
+    private EditText firstNumber, secondNumber;
+    private TextView result;
+
+    public static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+
+    private class ButtonClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            Integer firstValue = Integer.valueOf(firstNumber.getText().toString());
+            Integer secondValue = Integer.valueOf(secondNumber.getText().toString());
+            switch (v.getId()) {
+                case R.id.minus_button:
+                    if (isNumeric(firstNumber.getText().toString()) && isNumeric(secondNumber.getText().toString())) {
+                        Integer resuult = firstValue - secondValue;
+                        result.setText(firstNumber.getText().toString() + " - " + secondNumber.getText().toString() + " = " + resuult.toString());
+                    } else {
+                        Toast.makeText(getApplicationContext(), "One or both of the numbers are invalid !", Toast.LENGTH_LONG).show();
+                    }
+                    break;
+                case R.id.plus_button:
+                    if (isNumeric(firstNumber.getText().toString()) && isNumeric(secondNumber.getText().toString())) {
+                        Integer resuult = firstValue + secondValue;
+                        result.setText(firstNumber.getText().toString() + " + " + secondNumber.getText().toString() + " = " + resuult.toString());
+                    } else {
+                        Toast.makeText(getApplicationContext(), "One or both of the numbers are invalid !", Toast.LENGTH_LONG).show();
+                    }
+                    break;
+//                case R.id.go_to_secondary:
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test01_var03_main);
+
+        plusButton = (Button) findViewById(R.id.plus_button);
+        minusButton = (Button) findViewById(R.id.minus_button);
+        goToSecondary = (Button) findViewById(R.id.go_to_secondary);
+        firstNumber = (EditText) findViewById(R.id.first_number);
+        secondNumber = (EditText) findViewById(R.id.second_number);
+        result = (TextView) findViewById(R.id.result_text);
+
+        ButtonClickListener buttonClickListener = new ButtonClickListener();
+        plusButton.setOnClickListener(buttonClickListener);
+        minusButton.setOnClickListener(buttonClickListener);
+        goToSecondary.setOnClickListener(buttonClickListener);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString("firstNumber", firstNumber.getText().toString());
+        savedInstanceState.putString("secondNumber", secondNumber.getText().toString());
+        savedInstanceState.putString("result", result.getText().toString());
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        if (savedInstanceState.containsKey("firstNumber") && savedInstanceState.containsKey("secondNumber") && savedInstanceState.containsKey("result")) {
+            Toast.makeText(getApplicationContext(), "The saved values are: " + savedInstanceState.getString("firstNumber") + " " + savedInstanceState.getString("secondNumber") + " " + savedInstanceState.getString("result"), Toast.LENGTH_LONG).show();
+        }
+    }
+}
